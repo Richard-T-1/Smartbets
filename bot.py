3ac8e40c850ef5d669f43feecd16aeab5fce94dd
Richard-T-1 <risko.turcan@gmail.com>
2025-07-27 11:59:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -232,7 +232,7 @@ def send_ticket_to_channel():
     
     # Caption pre tiket
     caption = (f"🏆 {match_data['team1']} vs {match_data['team2']}\n"
-              f"🏐 {match_data['tournament']}\n"
+              f"🎾 {match_data['tournament']}\n"
               f"🕘 {match_data['time']}\n\n"
               f"🎯 {match_data['pick']}\n"
               f"💰 Kurz: {match_data['odds']}")
